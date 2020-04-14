176a91b0059414c6df24336f136c1a3775889760
Vijin Palazhi <vijin.palazhi@gmail.com>
2020-04-13 23:41:28 -0400

    new project files

--- mercuryProject/mercury/urls.py
+++ mercuryProject/mercury/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 import portfolios.views
@@ -22,6 +22,7 @@ import portfolios.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', portfolios.views.home, name='home'),
+    path('blog/', include('blog.urls'))
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
