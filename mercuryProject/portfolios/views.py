4a9ca0953c1279428de60b0a551eaab3fb8e5980
Vijin Palazhi <vijin.palazhi@gmail.com>
2020-04-13 22:24:01 -0400

    new project files

--- mercuryProject/portfolios/views.py
+++ mercuryProject/portfolios/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import Portfolios
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    portfolios = Portfolios.objects
+    return render(request, 'home.html', {'portfolios': portfolios})
